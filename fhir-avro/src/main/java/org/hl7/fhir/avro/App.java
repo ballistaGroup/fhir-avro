8b6e9102cc9fc4ece2d5733a969f9e30412d2a9b
Angelo Kastroulis <angelo@kastroulis.com>
2020-01-24 09:55:52 -0500

    initial commit

---
+++ fhir-avro/src/main/java/org/hl7/fhir/avro/App.java
@@ -0,0 +1,13 @@
+package org.hl7.fhir.avro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ readme.md
@@ -0,0 +1,3 @@
+To rebuild models:
+
+mvn clean package
